67a94ca46af28d92b2f9a6b3753212db3ad7feed
AbrahamUmana8427 <84608642+AbrahamUmana8427@users.noreply.github.com>
2022-06-30 12:15:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/barcustomerservice/Bar.java
@@ -0,0 +1,19 @@
+package com.example.barcustomerservice;
+
+public class Bar {
+
+    private int mesas;
+
+    public Bar(int mesas) {
+        this.mesas = mesas;
+    }
+
+    public int getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(int mesas) {
+        this.mesas = mesas;
+    }
+
+}
